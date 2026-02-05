f3fd110261320b0349167417d9558c0bbc7cf740
makarmee <makarmee@gmail.com>
2026-02-05 17:28:19 +0430

    customize HTML

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 
 class ReviewForm(forms.Form):
-  user_name=forms.CharField()
+  user_name=forms.CharField(label='Your Name',max_length=100,error_messages={
+    'required':'your name must not be empty',
+    'max_length':'please enter a shorter name'
+  })
   
