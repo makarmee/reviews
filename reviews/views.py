80c0f661b813c50192d95c357521594a7462f8f5
makarmee <makarmee@gmail.com>
2026-02-08 15:57:11 +0430

    use list-view

--- reviews/views.py
+++ reviews/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.views import View
 from django.views.generic.base import TemplateView
+from django.views.generic import ListView
 from .forms import ReviewForm
 from .models import Review
 
@@ -33,11 +34,20 @@ class ThankYouView(TemplateView):
     context['message']='This Works!'
     return context
   
-class ReviewsListView(TemplateView):
+class ReviewsListView(ListView):
   template_name='reviews/review_list.html'
-  def get_context_data(self, **kwargs) -> dict[str, Any]:
+  model=Review
+  context_object_name='reviews'
+  
+  
+class SingleReviewView(TemplateView):
+  template_name='reviews/single_review.html'
+  
+  def get_context_data(self, **kwargs):
       context = super().get_context_data(**kwargs)
-      reviews=Review.objects.all()
-      context["reviews"] = reviews
+      review_id=kwargs['id']
+      selected_review=Review.objects.get(pk=review_id)
+      context["review"] = selected_review
       return context
+  
   
