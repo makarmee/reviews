cd6ee65aa2e48c1c0f5e072f3e683bda3f6167cb
makarmee <makarmee@gmail.com>
2026-02-09 15:51:08 +0430

    add an image-field

--- profiles/views.py
+++ profiles/views.py
@@ -2,13 +2,9 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
 from .forms import ProfileForm
+from .models import UserProfile
 
 # Create your views here.
-
-def store_files(file):
-    with open('temp/image.jpg','wb+')as dest:
-        for chunk in file.chunks():
-            dest.write(chunk)
 
 class CreateProfileView(View):
     def get(self, request):
@@ -21,7 +17,8 @@ class CreateProfileView(View):
         submitted_form=ProfileForm(request.POST,request.FILES)
         
         if submitted_form.is_valid():
-            store_files(request.FILES['image'])
+            profile=UserProfile(image=request.FILES['user_image'])
+            profile.save()
             return HttpResponseRedirect('/profiles')
         
         return render(request,'profiles/create_profile.html',{
